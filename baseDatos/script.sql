0c036eb0b7ea532360570fe688e1bf844e489de7
Frank Laura Borja <frank.cg9@gmail.com>
2021-01-07 18:17:57 -0500

    first-commit

---
+++ baseDatos/script.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE retoIBM;
+
+USE retoIBM;
+
+CREATE TABLE retoTecnico (
+  sumando01 varchar(4) DEFAULT NULL,
+  sumando02 varchar(4) DEFAULT NULL,
+  resultado varchar(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
